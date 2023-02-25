0ef198445e791a606926c5cd87a4aa32396ecbf9
Michael Yang <sat.devops88@gmail.com>
2023-02-25 10:33:07 -0500

    clean code

--- src/components/Common/Loading/index.tsx
+++ src/components/Common/Loading/index.tsx
@@ -1,7 +1,5 @@
 export const Loading = () => {
   return (
-    // <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-20 backdrop-blur">
-    <div className="border-b-2 rounded-full border-primary h-9 w-9 animate-spin border-cyan-700" />
-    // </div>
+    <div className="border-primary h-9 w-9 animate-spin rounded-full border-b-2 border-cyan-700" />
   );
 };
